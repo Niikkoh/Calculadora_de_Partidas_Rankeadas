50e358115259b85734b1ed54ca118e911430b5da
Nicolas <nicolas.pereira2000vp@gmail.com>
2023-12-14 10:03:51 -0300

    format code

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 
 /*Constantes que definem o score mínimo necessário para alcançar o Rank*/
 
-const SCORE_FOR_BRONZE = 11
-const SCORE_FOR_SILVER = 21
-const SCORE_FOR_GOLD = 51
-const SCORE_FOR_DIAMOND = 81
-const SCORE_FOR_LEGENDARY = 91
-const SCORE_FOR_IMORTAL = 101
+const SCORE_FOR_BRONZE = 11;
+const SCORE_FOR_SILVER = 21;
+const SCORE_FOR_GOLD = 51;
+const SCORE_FOR_DIAMOND = 81;
+const SCORE_FOR_LEGENDARY = 91;
+const SCORE_FOR_IMORTAL = 101;
 
 /*Exemplo de ficha de um jogador*/
 
-const PLAYER_VICTORIES_MATCH_EXAMPLE = 63
-const PLAYER_DEFEAT_MATCH_EXAMPLE = 1
+const PLAYER_VICTORIES_MATCH_EXAMPLE = 63;
+const PLAYER_DEFEAT_MATCH_EXAMPLE = 1;
 
 /*=======================================================================*/
 
  /*Classe do jogador*/
 
-class player {
+class Player {
 
     constructor(victoriesMatch, defeatMatch)
     {
-        this.victoriesMatch = victoriesMatch
-        this.defeatMatch = defeatMatch
-        this.score = 0
-        this.rank = "indefinido"
+        this.victoriesMatch = victoriesMatch;
+        this.defeatMatch = defeatMatch;
+        this.score = 0;
+        this.rank = "indefinido";
     }
     
       /*Calcula o rank do jogador baseado em seu score*/
@@ -32,31 +32,31 @@ class player {
     setPlayerRank () 
     {
         if(this.score < SCORE_FOR_BRONZE)
-            this.rank = "Ferro"
+            this.rank = "Ferro";
         else if(this.score < SCORE_FOR_SILVER)
-            this.rank = "Bronze"
+            this.rank = "Bronze";
         else if(this.score < SCORE_FOR_GOLD)
-            this.rank = "Prata"
+            this.rank = "Prata";
         else if(this.score < SCORE_FOR_DIAMOND)
-            this.rank = "Ouro"
+            this.rank = "Ouro";
         else if(this.score < SCORE_FOR_LEGENDARY)
-            this.rank = "Diamante"
+            this.rank = "Diamante";
         else if(this.score < SCORE_FOR_IMORTAL)
-            this.rank = "Lendário"
+            this.rank = "Lendário";
         else
-            this.rank = "Imortal"
+            this.rank = "Imortal";
     }
 
         /*Imprime a saída na tela*/
 
     showPlayerRank ()
     {
-        console.log (`O Herói tem de saldo de ${this.score} está no nível de ${this.rank}`)
+        console.log (`O Herói tem de saldo de ${this.score} está no nível de ${this.rank}`);
     }
 
         /*Calcula o score do jogador através do calculo 'vitórias - derrotas' */
 
-    setPlayerScore () { this.score = this.victoriesMatch - this.defeatMatch }
+    setPlayerScore () { this.score = this.victoriesMatch - this.defeatMatch; }
 }
 
 main()
@@ -67,15 +67,15 @@ function main()
 {
    
     /*Instanciando objeto jogador*/
-    let onePlayer = new player(PLAYER_VICTORIES_MATCH_EXAMPLE, PLAYER_DEFEAT_MATCH_EXAMPLE)
+    let onePlayer = new Player(PLAYER_VICTORIES_MATCH_EXAMPLE, PLAYER_DEFEAT_MATCH_EXAMPLE);
     
     /*Define o score e o rank do jogador instanciado*/
 
-    onePlayer.setPlayerScore()
-    onePlayer.setPlayerRank()
+    onePlayer.setPlayerScore();
+    onePlayer.setPlayerRank();
 
     /*Imprime a saída*/
     
-    onePlayer.showPlayerRank()
+    onePlayer.showPlayerRank();
 
 }
